0d4f4ea61d438d8f3dbcc55bcc5e287e4f6b83bc
Ricardo-Sousa <rwmsousa@gmail.com>
2021-08-03 19:20:02 -0300

    requisito 6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -9,8 +9,8 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
+    <div>
+      <BrowserRouter>
         <Switch>
           <Route path="/movies/new" component={ NewMovie } />
           <Route path="/movies/:id/edit" component={ EditMovie } />
@@ -18,9 +18,8 @@ function App() {
           <Route exact path="/" component={ MovieList } />
           <Route path="*" component={ NotFound } />
         </Switch>
-        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
